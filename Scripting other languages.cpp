f86e90acee74947d0f9a65b17fe8b4459edd3441
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-05-20 10:20:46 +0300

    Testing math operations and assignment

--- Scripting other languages.cpp
+++ Scripting other languages.cpp
@@ -2,24 +2,6 @@
 #include "Script.h"
 #include <conio.h>
 
-bool foo()
-{
-	std::cout << "\n\n\nHello world (but cool)!";
-	return false;
-}
-
-bool boo()
-{
-	std::cout << "\n\n\nHello world (but not that cool)!";
-	return false;
-}
-
-bool goo()
-{
-	std::cout << "\n\n\nHello world (but it is alright)!";
-	return false;
-}
-
 std::istream& getline(std::istream& istream, std::stringstream& stringstream)
 {
 	std::string line;
@@ -28,24 +10,76 @@ std::istream& getline(std::istream& istream, std::stringstream& stringstream)
 	return istream;
 }
 
-
-
 int main()
 {
 	Function_Mapper func_map;
-	func_map.Add("testA", foo);
-	func_map.Add("testB", boo);
-	func_map.Add("testC", goo);
+	
+	int num1 = 0;
+	int num2 = 0;
+
+	bool is_value_given = false;
+
+	auto give = [&is_value_given]
+	{
+		is_value_given = true;
+	};
+
+	auto assign = [&num2](const int val)
+	{
+		num2 = val;
+	};
+
+	auto addition = [&num1, &num2]
+	{
+		num1 += num2;
+	};
+
+	auto subtraction = [&num1, &num2]
+	{
+		num1 -= num2;
+	};
+
+	auto multiplication = [&num1, &num2]
+	{
+		num1 *= num2;
+	};
+
+	auto division = [&num1, &num2]
+	{
+		num1 /= num2;
+	};
+
+	auto print = [&num1]
+	{
+		std::cout << "\n\t\t\t---{" << num1 << "}---\n";
+	};
+
+	func_map.Add("add", addition);
+	func_map.Add("subtract", subtraction);
+	func_map.Add("multiply", multiplication);
+	func_map.Add("divide", division);
+	func_map.Add("print", print);
+	func_map.Add("give", give);
+	func_map.Add("assign");
+
 	bool exit = false;
 
-	std::list<std::function<void()>> list;
 	std::stringstream line;
 	getline(std::cin, line);
 	std::string word;
 
 	while (line >> word)
+	{
+		is_value_given = false;
 		if (word == "exit" || !func_map.Call(word))
 			break;
+		if (is_value_given)
+		{
+			line >> word;
+			auto change = std::bind(assign, std::stoi(word));
+			func_map.Change("assign", change);
+		}
+	}
 
 	while (!_kbhit());
 	return 0;
