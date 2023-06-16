441c4ce4b8e9209dd3f4a18c61786694413c3ad4
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-06-16 23:53:02 +0300

    Added exceptions that have error numbers at the beginning, added a run_engine function that uses the console instead of a file

--- Script.h
+++ Script.h
@@ -141,29 +141,75 @@ public:
 			}
 		}
 	}
+
+	// This function starts parsing an input from the console to execute the script
+	void run_engine()
+	{
+		auto assign = [&](const double& val)
+		{
+			set_memory("num2", val);
+		};
+
+		std::stringstream line;
+		getline(std::cin, line);
+		std::string word;
+
+		while (line >> word)
+		{
+			set_memory("is_value_given", -1.0);
+
+			if (word == "exit" || !func.Call(word))
+				break;
+			if (get_memory("is_value_given") > 0.0)
+			{
+				line >> word;
+				auto change = std::bind(assign, std::stoi(word));
+				func.Change("assign", change);
+				func.Call("assign");
+			}
+		}
+	}
 private:
+	// A way to turn a line of std::cin or any other input stream into a stringstream
+	// @param istream This is for the std::cin to be used, can also use any other input stream
+	// @param stringstream This is used to store the std::cin line of contents
+	// @return This is used to return the new state that the parameter istream is at
+	std::istream& getline(std::istream& istream, std::stringstream& stringstream)
+	{
+		std::string line;
+		std::getline(istream, line);
+		stringstream.str(line);
+		return istream;
+	}
+
 	// Helper function to assign a value to piece in memory as well as making a pointer for it
 	// @param name Used for the name of the variable
 	// @param initial_val Used as the initial value of the variable
-	// @return True if the number of variables are less than memory
-	const bool allocate_memory(const std::string name, double initial_val = 0.0)
+	const size_t allocate_memory(const std::string name, double initial_val = 0.0)
 	{
-		static size_t top = (size_t)-1;
-		if (++top == memory.size()) return false;
+		static size_t top = size_t(-1);
+		if (++top == memory.size()) 
+			throw std::exception("2 Allocation overflow");
 
 		auto [it, inserted] = pointers.emplace(name, top);
-		if (!inserted) top--;
+		if (!inserted) 
+		{
+			top--;
+			throw std::exception("3 Allocation duplicate, cannot duplicate allocation");
+		}
 
-		memory[std::distance(pointers.begin(),it)] = initial_val;
-
-		return true;
+		memory[top] = initial_val;
+		return top;
 	}
+
 	// Helper function so that the look up is seemless
 	// @param name Used for the look up of the variable
 	// @return The variable in question
 	inline const double get_memory(std::string name) const
 	{
-		return memory.at(pointers.at(name));
+		if (auto it = pointers.find(name); it != pointers.end())
+			return memory.at(it->second);
+		throw std::exception(std::string("0 Bad memory get error; the supposed " + name + " does not exist").c_str());
 	}
 
 	// Helper function so that setting a variable is seemless
@@ -171,7 +217,10 @@ private:
 	// @param val Used for the new value of the variable
 	inline void set_memory(const std::string& name, double val)
 	{
-		memory[pointers[name]] = val;
+		if (auto it = pointers.find(name); it != pointers.end())
+			memory[it->second] = val;
+		else
+			throw std::exception(std::string("1 Bad memory set error; the supposed " + name + " does not exist").c_str());
 	}
 private:
 	// A 32 Kilobyte storage capacity
--- Scripting other languages.cpp
+++ Scripting other languages.cpp
@@ -1,20 +1,11 @@
-#include <iostream>
 #include "Script.h"
 #include <conio.h>
-
-std::istream& getline(std::istream& istream, std::stringstream& stringstream)
-{
-	std::string line;
-	std::getline(istream, line);
-	stringstream = std::stringstream(line);
-	return istream;
-}
 
 int main()
 {
 	Engine eng;
 
-	eng.run_engine("Scripting.txt");
+	eng.run_engine();
 
 	while (!_kbhit());
 	return 0;
