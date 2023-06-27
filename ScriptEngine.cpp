22df002f4d1a27b49b99a2d14a005f6f1d50604c
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-06-27 06:48:46 +0300

    Some tweaking later, moved definitions, removed the script_error class and lastly made an enum with a struct to make data type getting faster

--- ScriptEngine.cpp
+++ ScriptEngine.cpp
@@ -1,1 +1,14 @@
 #include "ScriptEngine.h"
+
+inline ScriptEngine& ScriptEngine::set_var(std::string name, DataType value)
+{
+	variables[name] = value;
+	return *this;
+}
+
+inline ScriptEngine::ValueType ScriptEngine::get_var(std::string name)
+{
+	if (auto it = variables.find(name); it != variables.end())
+		return ValueType{it->second, Type(it->second.index())};
+	throw std::runtime_error("Variable does not exist");
+}
--- ScriptEngine.h
+++ ScriptEngine.h
@@ -12,35 +12,29 @@
 class ScriptEngine
 {
 public:
+	enum Type
+	{
+		Boolean,
+		Char,
+		Int,
+		Double,
+		String,
+		Count
+	};
 	using DataType = std::variant<bool, char, int, double, std::string>;
+	struct ValueType
+	{
+		DataType value;
+		Type type;
+	};
+public:
 	ScriptEngine()
 	{
 	}
-	ScriptEngine& set_var(std::string name, DataType value)
-	{
-		variables[name] = value;
-	}
-	DataType get_var(std::string name)
-	{
-		if (auto it = variables.find(name); it != variables.end())
-			return it->second;
-		throw script_error("var not found", 0);
-	}
+	ScriptEngine& set_var(std::string name, DataType value);
+	ValueType get_var(std::string name);
 private:
+	
 	std::unordered_map<std::string, DataType> variables;
 	std::string path;
-};
-
-class script_error : public std::runtime_error
-{
-public:
-	script_error() = default;
-	script_error(std::string msg) : std::runtime_error(msg) {}
-	script_error(std::string msg, int err_code) : std::runtime_error(msg), err_code(err_code) {}
-	const int which() const
-	{
-		return err_code;
-	}
-private:
-	int err_code;
 };
