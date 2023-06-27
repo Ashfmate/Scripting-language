d3e92d149dfc3a885ac3f1083cb881a41c519b82
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-06-27 07:18:12 +0300

    Testing the get_type function

--- Scripting other languages.cpp
+++ Scripting other languages.cpp
@@ -1,5 +1,6 @@
 #include "ScriptEngine.h"
 #include <conio.h>
+#include <fmt\format.h>
 
 int main()
 {
@@ -7,7 +8,31 @@ int main()
 
 	eng.run_engine("Scripting.txt");*/
 
+	ScriptEngine eng;
 
+	eng.set_var("double", double(5.0));
+
+	switch (eng.get_type(eng.get_var("double")))
+	{
+	case ScriptEngine::Boolean:
+		fmt::print("This some good bool");
+		break;
+	case ScriptEngine::Char:
+		fmt::print("This some good char");
+		break;
+	case ScriptEngine::Double:
+		fmt::print("This some good double");
+		break;
+	case ScriptEngine::Int:
+		fmt::print("This some good int");
+		break;
+	case ScriptEngine::String:
+		fmt::print("This some good string");
+		break;
+	default:
+		fmt::print("This some good Nothing");
+		break;
+	}
 
 	while (!_kbhit());
 	return 0;
