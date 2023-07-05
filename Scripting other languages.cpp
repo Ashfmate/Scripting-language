2786a26b74ba3f6fc3b74044fb2748caffe55504
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-07-05 23:44:18 +0300

    Cleaned up house

--- Scripting other languages.cpp
+++ Scripting other languages.cpp
@@ -1,6 +1,7 @@
 #include "ScriptEngine.h"
 #include <fmt\core.h>
-
+import Utils;
+import Error;
 
 int main()
 {
@@ -10,10 +11,7 @@ int main()
 	// *) Add functions
 	// *) Add Classes (The hardest of them all)
 	// *) Make a var class in code that will allow for any class to be accepted
-	ScriptEngine eng;
-	auto res = eng.start("Scripting.txt");
-	eng.exec();
-	fmt::print("\n\n\n\n{} : Error Code [{}]\n", res.what(), res.which());
+
 	system("pause");
 	return 0;
 }
